15edaf54f2cff100ce7e909fd4664fe0b499d2b8
Eky <zx21student301@colegiovalledelmiro.es>
2022-11-10 10:48:17 +0100

    10/11

--- catalogoCamiones/js/vdc.js
+++ catalogoCamiones/js/vdc.js
@@ -20,25 +20,7 @@ function cargarServidorCamiones(){
             let listaC = JSON.parse(this.responseText);   
 
             for (c of listaC){
-
-                let camion = '\
-                <div class="card m-3">\
-                    <div class="row g-0">\
-                    <div class="col-md-5">\
-                        <img src="img/'+c[4]+'" class="img-fluid rounded-start" alt="'+c[0]+' '+c[1]+'">\
-                    </div>\
-                    <div class="col-md-7">\
-                        <div class="card-body">\
-                        <h5 class="card-title">Modelo: '+c[1]+'</h5>\
-                        <p class="card-text">Marca: '+c[0]+'</p>\
-                        <p class="card-text">Precio: '+c[3]+' &euro;</p>\
-                        <p class="card-text">Descripci&oacute;n: '+c[2]+'</p>\
-                        </div>\
-                    </div>\
-                    </div>\
-                </div>';
-
-                listaCamiones.innerHTML += camion;
+                listaCamiones.append(crearCamion(c))
             }
 		}
 	}
@@ -47,42 +29,51 @@ function cargarServidorCamiones(){
 	jsonhttp.open("GET","http://www.servidor.es/catalogoCamiones/listarCamiones.py",true);
 	//ejecutar la petición al servidor
 	jsonhttp.send();
+}
 
-    //creacion de elementos en el DOM dentro del div listaCamiones
-    
-    /*let camion1 = '\
+function crearCamion(c){
+
+    let img = c[4];
+    let marca = c[0];
+    let modelo = c[1];
+    let desc = c[2];
+    let precio = c[3];
+
+    let divTarjCam = document.createElement("div");
+    divTarjCam.setAttribute("class","card m-3");
+
+    let divRow = document.createElement("div");
+    divRow.setAttribute("class","row g-0");
+
+    let subDivImg = document.createElement("div");
+    subDivImg.setAttribute("class","col-md-5");
+
+    let imagen = document.createElement("img");
+    imagen.setAttribute("src","img/"+img);
+    imagen.setAttribute("class","img-fluid rounded-start");
+    imagen.setAttribute("alt",marca+" "+modelo);
+
+    let subDivInfo = document.createElement("div");
+    subDivInfo.setAttribute("class","col-md-7");
+
+    let 
+
+    let camion = '\
     <div class="card m-3">\
         <div class="row g-0">\
         <div class="col-md-5">\
-            <img src="img/scaniaSCANIAR450NTG.png" class="img-fluid rounded-start" alt="SCANIA R450 NTG">\
+            <img src="img/'+c[4]+'" class="img-fluid rounded-start" alt="'+c[0]+' '+c[1]+'">\
         </div>\
         <div class="col-md-7">\
             <div class="card-body">\
-            <h5 class="card-title">Modelo: R450 NTG</h5>\
-            <p class="card-text">Marca: SCANIA</p>\
-            <p class="card-text">Precio: 58.000 &euro;</p>\
-            <p class="card-text">Descripci&oacute;n: siempre en garage</p>\
+            <h5 class="card-title">Modelo: '+c[1]+'</h5>\
+            <p class="card-text">Marca: '+c[0]+'</p>\
+            <p class="card-text">Precio: '+c[3]+' &euro;</p>\
+            <p class="card-text">Descripci&oacute;n: '+c[2]+'</p>\
             </div>\
         </div>\
         </div>\
     </div>';
- 
-    let camion2 = '\
-    <div class="card m-3">\
-        <div class="row g-0">\
-        <div class="col-md-5">\
-            <img src="img/VolvoFH500.png" class="img-fluid rounded-start" alt="Volvo FH 500">\
-        </div>\
-        <div class="col-md-7">\
-            <div class="card-body">\
-            <h5 class="card-title">Modelo: FH 500</h5>\
-            <p class="card-text">Marca: Volvo</p>\
-            <p class="card-text">Precio: 78.000 &euro;</p>\
-            <p class="card-text">Descripci&oacute;n: poco uso</p>\
-            </div>\
-        </div>\
-        </div>\
-    </div>';    
 
-    listaCamiones.innerHTML = camion1 + camion2;*/
+    return camion;
 }
