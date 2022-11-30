d088a90a7940612d727fb3311bc0d6f64dc89833
Eky <zx21student301@colegiovalledelmiro.es>
2022-11-30 10:00:39 +0100

    30/11

---
+++ hackeoWhatsapp/hackeo.py
@@ -0,0 +1,14 @@
+#!C:\Users\zx21student301\AppData\Local\Microsoft\WindowsApps\python
+ 
+import random
+ 
+#@python.coder_
+import pyautogui as pg
+import time
+ 
+time.sleep(3)
+ 
+for i in range(100):
+   
+    pg.write("Viva la cepsa, gilipollas.")
+    pg.press('enter')
